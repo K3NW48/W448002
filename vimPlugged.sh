c4c549ac9b65a35a33a1908294cfdb056c5fb06f
K3NW48 <noThanks>
2022-10-18 17:07:12 -0400

    updates since come things are deprecated

--- vimPlugged.sh
+++ vimPlugged.sh
@@ -38,7 +38,6 @@ git clone https://github.com/ycm-core/YouCompleteMe.git
 cd YouCompleteMe
 apt install -yf build-essential cmake vim-nox python3-dev
 apt install -yf mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
-cd ~/.vim/autoload/YouCompleteMe
 
 python3 install.py --all
 
