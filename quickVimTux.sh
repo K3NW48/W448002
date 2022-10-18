91b33988c504838ea4bddc61f8e021a32db9b3ba
K3NW48 <noThanks>
2022-10-18 01:30:35 -0400

    Vim and tmux are nice

---
+++ quickVimTux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt install -yf vim tmux
+
+mv .vimrc ~/
+
+echo ' '
+echo 'Your vim config is only for this user currently. cp .vimrc to /etc/vim//vimrc.local for Ubuntu/Debian systems and /etc/vimrc for CentOS/RHEL7'
+sleep 3y
+
+
