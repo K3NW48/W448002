0a51d961d9c1051b21283228b3ada226946c53fb
K3NW48 <noThanks>
2022-10-18 01:32:09 -0400

    sleep fix

--- quickVimTux.sh
+++ quickVimTux.sh
@@ -6,6 +6,4 @@ mv .vimrc ~/
 
 echo ' '
 echo 'Your vim config is only for this user currently. cp .vimrc to /etc/vim//vimrc.local for Ubuntu/Debian systems and /etc/vimrc for CentOS/RHEL7'
-sleep 3y
-
-
+sleep 3
