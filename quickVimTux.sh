aecaf0c3d7db99cc62b172092ffc12a2a2f8b4f5
K3NW48 <noThanks>
2022-10-18 01:55:45 -0400

    fixes

--- quickVimTux.sh
+++ quickVimTux.sh
@@ -2,7 +2,7 @@
 
 apt install -yf vim tmux
 
-mv .vimrc $HOME
+cp .vimrc $HOME
 
 echo ' '
 echo 'Your vim config is only for this user currently. cp .vimrc to /etc/vim//vimrc.local for Ubuntu/Debian systems and /etc/vimrc for CentOS/RHEL7'
