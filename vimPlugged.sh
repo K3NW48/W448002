56b0615c3d23ebbf841e09c33cfb8a201a1c64b6
K3NW48 <noThanks>
2022-10-18 20:39:51 -0400

    finally got something here

--- vimPlugged.sh
+++ vimPlugged.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt install -yf curl
+apt install -yf curl vim tmux
 
 COME_BACK_HERE=${PWD}
 
@@ -10,35 +10,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cd ~/.vim/autoload
 
-cat << EOF > ~/.vimrc 
-call plug#begin()
-
-Plug 'ycm-core/YouCompleteMe'
-
-call plug#end()
-set mouse=a
-colorcheme industry
-set spell
-set title
-set cursorline
-set number
-set ruler
-syntax enable
-set hlsearch
-set ignorecase
-set smartcase
-set history=4300
-set smarttab
-set tabspace=2
-EOF
-
-cp $COME_BACK_HERE/.vimrc ~/.vimrc
-
-git clone https://github.com/ycm-core/YouCompleteMe.git
-cd YouCompleteMe
-apt install -yf build-essential cmake vim-nox python3-dev
-apt install -yf mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
-
-python3 install.py --all
-
 cd $COME_BACK_HERE
+
+cp .vimrcPlugged ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
+echo ' '
+echo '..Aanii gaye Boozhoo Waabooz...  =,='
+echo ' '
